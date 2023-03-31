ce58d1b415c8f6cc05e70bdc44c0069c2f6b3d00
Philipp Kerling <pkerling@casix.org>
2023-04-01 00:09:05 +0200

    Fix update_config.sh

--- update_config.sh
+++ update_config.sh
@@ -1,16 +1,16 @@
-cd /data
+cd /data/hebbot-config
 
 echo "Updating config.toml..."
-wget -nv -O config.toml https://mydomain.com/hebbot/config.toml
+wget -nv -O config.toml https://raw.githubusercontent.com/xbmc/hebbot-config/main/config.toml
 
 echo "Updating report_template.md..."
-wget -nv -O report_template.md https://mydomain.com/hebbot/report_template.md
+wget -nv -O report_template.md https://raw.githubusercontent.com/xbmc/hebbot-config/main/report_template.md
 
 echo "Updating section_template.md..."
-wget -nv -O section_template.md https://mydomain.com/hebbot/section_template.md
+wget -nv -O section_template.md https://raw.githubusercontent.com/xbmc/hebbot-config/main/section_template.md
 
 echo "Updating project_template.md..."
-wget -nv -O project_template.md https://mydomain.com/hebbot/project_template.md
+wget -nv -O project_template.md https://raw.githubusercontent.com/xbmc/hebbot-config/main/project_template.md
 
-echo "Updating update_config.sh..."
-wget -nv -O update_config.sh https://mydomain.com/hebbot/update_config.sh
+#echo "Updating update_config.sh..."
+#wget -nv -O update_config.sh https://raw.githubusercontent.com/xbmc/hebbot-config/main/update_config.sh
